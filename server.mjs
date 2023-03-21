e8a7d2b5ece3e9dbcfd94c9fd554653377ce42d8
h.oda <62193961+hiroic-works@users.noreply.github.com>
2023-03-21 18:32:52 +0900

    Prisma init & User CRUD

--- server.mjs
+++ server.mjs
@@ -1,10 +1,13 @@
 import express from "express";
 import cors from "cors";
+import { PrismaClient } from "@prisma/client";
 import env from "dotenv";
 env.config();
 
 const app = express();
 const port = process.env.PORT || 8080;
+
+const prisma = new PrismaClient();
 
 app.use(cors());
 
@@ -12,11 +15,115 @@ app.use(express.json());
 
 app.get("/", (req, res) => res.json({ msg: "Hello World" }));
 
+app.get("/api/users", async (req, res, next) => {
+  try {
+    const users = await prisma.user.findMany();
+    res.json(users);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+app.get("/api/users/:id", async (req, res, next) => {
+  const { id } = req.params;
+  const _id = parseInt(id);
+
+  const user = await prisma.user.findUnique({
+    where: {
+      id: _id,
+    },
+  });
+
+  if (!user) return res.status(400).json({ msg: "存在しないユーザーです" });
+
+  res.json(user);
+});
+
+app.post("/api/users", async (req, res, next) => {
+  const { name, email } = req.body;
+
+  // TODO: request check
+
+  try {
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        // posts: {
+        //   create: {
+        //     title: "Hello World",
+        //     content: "test content",
+        //   },
+        // },
+      },
+    });
+    res.status(201).json({ msg: "User created!" });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+app.patch("/api/users/:id", async (req, res, next) => {
+  const { id } = req.params;
+  const _id = parseInt(id);
+  const { name } = req.body;
+
+  // user check
+  const user = await prisma.user.findUnique({
+    where: {
+      id: _id,
+    },
+  });
+  if (!user) return res.status(400).json({ msg: "存在しないユーザーです" });
+
+  try {
+    const user = await prisma.user.update({
+      where: {
+        id: _id,
+      },
+      data: {
+        name,
+      },
+    });
+    res.json({ msg: "User updated!" });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+app.delete("/api/users/:id", async (req, res, next) => {
+  const { id } = req.params;
+  const _id = parseInt(id);
+
+  // user check
+  const user = await prisma.user.findUnique({
+    where: {
+      id: _id,
+    },
+  });
+  if (!user) return res.status(400).json({ msg: "存在しないユーザーです" });
+
+  try {
+    const user = await prisma.user.delete({
+      where: {
+        id: _id,
+      },
+    });
+    res.json({ msg: "User deleted!" });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
 // error handle
-app.use( (req, res) => {
+app.use((req, res) => {
   res.status(404).json({ msg: "ページが存在しません。" });
 });
-app.use( (err, req, res, next) => {
+app.use((err, req, res, next) => {
   if (res.headersSent) {
     return next(err);
   }
